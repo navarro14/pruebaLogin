71376c6740b84c3243e4b32444a11f783ad9f6d9
navarro14 <angel783041l@gmail.com>
2025-02-28 09:44:47 -0600

    primer commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/stacks/screens/Login';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Login/>
+    </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
